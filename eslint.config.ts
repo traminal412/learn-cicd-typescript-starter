77e791a4429d136c6f5eec7fd9ac5596202afd03
traminal412 <tramanh.tl@gmail.com>
2025-02-15 10:58:39 -0800

    add security checks

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,13 +1,17 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import pluginSecurity from "eslint-plugin-security";
 
+/** @type { import("eslint").Linter.Config[] } */
 export default [
   { files: ["**/*.{ts}"] },
   { languageOptions: { globals: globals.browser } },
   {
     ignores: ["dist/*", "coverage/*"],
   },
+
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
 ];
